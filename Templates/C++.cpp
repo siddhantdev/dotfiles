04377496bc18d614e9f0dddc7dd275b7a9791a4e
Siddhant Agarwal <agarwalsiddhant53@gmail.com>
2021-12-06 13:28:03 +0530

    added template files

---
+++ Templates/C++.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+typedef vector<ll> vi;
+typedef pair<ll, ll> pi;
+
+#define FOR(i, j, k) for (ll i = j; i < (ll) k; ++i)
+#define FORD(i, j, k) for (ll i = j; i >= (ll) k; --i)
+#define nl "\n"
+#define all(x) (x).begin(), (x).end()
+
+#define sc second
+#define fr first
+#define pb push_back
+
+void solve()
+{
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    ll t = 1;
+    // cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+}
---
+++ Templates/Markdown.md
@@ -0,0 +1,15 @@
+---
+title:
+subtitle:
+author: Siddhant Agarwal
+toc: true
+include-before: |
+    \pagenumbering{gobble}
+    \newpage
+    \pagenumbering{roman}
+urlcolor: blue
+geometry: margin=2.6cm
+---
+
+\newpage
+\pagenumbering{arabic}
